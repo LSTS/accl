2a714130a3bb2ce0952be5fa1ec7fbb584cd4941
José Miguel Loureiro <jose.miguel.loureiro@gmail.com>
2015-07-02 10:08:23 +0100

    Removed All packages from ASA fork.

--- src/pt/lsts/accl/listenners/LocationChangeListener.java
+++
@@ -1,8 +0,0 @@
-package pt.lsts.accl.listenners;
-
-import android.location.Location;
-
-public interface LocationChangeListener {
-
-	public void onLocationChange(Location location);
-}
--- src/pt/lsts/accl/listenners/MainSysChangeListener.java
+++
@@ -1,8 +0,0 @@
-package pt.lsts.accl.listenners;
-
-import pt.lsts.accl.sys.Sys;
-
-public interface MainSysChangeListener {
-
-	public void onMainSysChange(Sys newMainSys);
-}
