cc5b398604bac82774c0ffbec43b8e24a8aa4c2f
José Miguel Loureiro <jose.miguel.loureiro@gmail.com>
2015-07-06 13:31:47 +0100

    Added Events for Generic and Main System Updates

---
+++ src/pt/lsts/accl/event/EventMainSystemUpdated.java
@@ -0,0 +1,24 @@
+package pt.lsts.accl.event;
+
+import pt.lsts.accl.sys.Sys;
+
+public class EventMainSystemUpdated extends AbstractACCLEvent {
+
+	private Sys sys;
+	
+	public EventMainSystemUpdated(Sys sys) {
+		this.sys = sys;
+	}
+
+	/**
+	 * @return the system
+	 */
+	public final Sys getSys() {
+		return sys;
+	}
+	
+	@Override
+	public String toString() {
+		return super.toString() + " - "+sys.getName();
+	}	
+}
---
+++ src/pt/lsts/accl/event/EventSystemUpdated.java
@@ -0,0 +1,24 @@
+package pt.lsts.accl.event;
+
+import pt.lsts.accl.sys.Sys;
+
+public class EventSystemUpdated extends AbstractACCLEvent {
+
+	private Sys sys;
+	
+	public EventSystemUpdated(Sys sys) {
+		this.sys = sys;
+	}
+
+	/**
+	 * @return the system
+	 */
+	public final Sys getSys() {
+		return sys;
+	}
+	
+	@Override
+	public String toString() {
+		return super.toString() + " - "+sys.getName();
+	}	
+}
