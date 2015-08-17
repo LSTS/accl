8d1092ee51cbb8635f418401db24b582bd44b90c
José Miguel Loureiro <jose.miguel.loureiro@gmail.com>
2015-08-17 14:13:57 +0100

    Added onCreateView method with FragmentID as arg.

--- androidlib/src/main/java/pt/lsts/accl/androidlib/AcclFragment.java
+++ androidlib/src/main/java/pt/lsts/accl/androidlib/AcclFragment.java
@@ -1,22 +1,43 @@
 package pt.lsts.accl.androidlib;
 
+
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+
 /**
  * A placeholder fragment containing a simple view.
  */
 public class AcclFragment extends Fragment {
 
+    public static String TAG = "TAG";
+    public View v;
+
     public AcclFragment() {
+        TAG = this.getClass().getSimpleName();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_accl, container, false);
+        return onCreateView(inflater,container, R.layout.fragment_accl);
     }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+    }
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, int fragmentID){
+        return inflater.inflate(fragmentID, container, false);
+    }
+
 }
